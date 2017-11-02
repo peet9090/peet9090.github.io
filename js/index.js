5b61c700f791210a09752f81c3b3d23221ebe23c
Peet9090 <peet9090@hotmail.com>
2017-11-02 03:29:25 +0100

    Laatste update tot Feedback/beoordeling

--- js/index.js
+++ js/index.js
@@ -3,21 +3,11 @@
 	versie: 1.3 
 	Datum: 25 september 2017 */
 
-/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+console.log('test test test')
 
-// Functie die uitgevoerd wordt zodra je op het element met "da" als ID klikt
+// Functie die uitgevoerd wordt zodra je op het element met "inlog" als ID klikt
 document.getElementById("inlog").onclick = function() {
-	 // Laat het element zien met "popup" als ID door display op block te zetten (staat nu op display: none;)
-	 document.getElementById("bg").style.display = "block";
-	 document.getElementById("popup").style.display = "block";
-	;}
-
-// Functie die uitgevoerd wordt zodra je op het element met "close" als ID klikt
-document.getElementById("bg").onclick = function() {
-	// Bron: http://stackoverflow.com/questions/5480945/refreshing-page-on-click-of-a-button
-	document.getElementById("bg").style.display = "none";
-	 document.getElementById("popup").style.display = "none";
-};
-
-
+	 // Laat het element zien met "melding" als ID door display op block te zetten (staat nu op display: none;)
+	 document.getElementById("melding").style.display = "block";
+	};
 
