f809a911f86b87dd871e91fb08a476e47b30eb90
Peet9090 <peet9090@hotmail.com>
2017-11-09 00:07:28 +0100

    Met micro

--- js/index.js
+++ js/index.js
@@ -1,13 +1,40 @@
 /* Hoofd auteur: Peet Neunzig 
 	klas: 205 
-	versie: 1.3 
-	Datum: 25 september 2017 */
+	versie: 1.4 
+	Datum: 1 november 2017 */
 
-console.log('test test test')
+console.log('test test test');
 
-// Functie die uitgevoerd wordt zodra je op het element met "inlog" als ID klikt
 document.getElementById("inlog").onclick = function() {
+// Functie die uitgevoerd wordt zodra je op het element met "inlog" als ID klikt
+	if(document.getElementById("melding").classList.contains('block')){
+		document.getElementById("melding").classList.remove('block');
+	} else {
+		document.getElementById("melding").classList.add('block');
+	}
+}
+
+
+var urlMenu = document.getElementById('sorteerMenu');
+urlMenu.onchange = function(){
+	var userOption = this.options[this.selectedIndex];
+	if (userOption.value !="nothing"){
+		window.open(userOption.value, "Aantal Hartjes");
+	}
+}
+
+
+document.getElementById("hart").addEventListener("click", hart);
+function hart() {	
+	document.getElementById("hart").classList.add("fa-heart");
+    document.getElementById("hart").classList.remove("fa-heart-o");
+}
+
+document.getElementById("email").addEventListener("click", myScript);	
+function myScript() {	
+
 	 // Laat het element zien met "melding" als ID door display op block te zetten (staat nu op display: none;)
-	 document.getElementById("melding").style.display = "block";
-	};
+	 document.getElementById("letop").classList.add("block");
+};
+
 
